71024ab93e90d6e84bceea93c4b30a13ca7ba6d8
lvjihong <lvjihong2017@qq.com>
2025-05-09 15:34:10 +0800

    bat push

--- src/lua/ghub-sjz.lua
+++ src/lua/ghub-sjz.lua
@@ -1,6 +1,6 @@
 -- 鼠标平滑移动到右侧边缘的宏
 function OnEvent(event, arg)
-    OutputLogMessage("event:" .. event .. ",arg:" .. arg .. "\n")
+    --OutputLogMessage("event:" .. event .. ",arg:" .. arg .. "\n")
     if event == "MOUSE_BUTTON_PRESSED" and arg == 11 then -- 假设使用侧键5触发
         del_df()
     end
@@ -10,27 +10,35 @@ end
 function del_df()
     -- 获取当前位置
     local currentX, currentY = GetMousePosition()
+    OutputLogMessage("currentX:" .. currentX .. ",currentY:" .. currentY .. "\n")
+    PressMouseButton(1)
     -- 移动到屏幕最右边
+    Sleep(20)
     SmoothMove()
     Sleep(10)
+    ReleaseMouseButton(1)
+    Sleep(20)
     -- 移动回来
     MoveMouseTo(currentX, currentY)
 end
 
 -- 移动到屏幕最右边
 function SmoothMove()
+    Sleep(8)
     local num = 0
     --移动过程
     while true and num < 10 do
         num = num + 1
-        MoveMouseRelative(800, 0)
-        Sleep(10)
+        MoveMouseRelative(2000, 0)
+        Sleep(20)
         local currentX, currentY = GetMousePosition() -- 获取当前鼠标位置
-        if currentX >= 65535 then
+        if currentX >= 65000 then
             break
         end
     end
     --移动到最终目标点
-    MoveMouseTo(65535, 32767)
-    Sleep(10)
+    endX = math.random(64390, 64820)
+    endY = math.random(32750, 32780)
+    MoveMouseTo(endX, endY)
+    Sleep(8)
 end
